3a771a294d4324925386b172e7f6e53e9ae7a745
JarmanjitSingh <jarmanjits176@gmail.com>
2024-02-04 00:38:29 +0530

    home page done

--- Frontend/src/components/header.tsx
+++ Frontend/src/components/header.tsx
@@ -1,11 +1,55 @@
+import { useState } from "react";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
+const user = { _id: "dd", role: "admin" };
 
 const Header = () => {
-  return (
-    <div>
-      header
-    </div>
-  )
-}
+    const [isOpen, setIsOpen] = useState<boolean>(false)
 
-export default Header
+    const logoutHandler = ()=>{
+        setIsOpen(false)
+    }
+  return (
+    <nav className="header">
+      <Link to={"/"} onClick={()=> setIsOpen(false)}>Home</Link>
+      <Link to={"/search"} onClick={()=> setIsOpen(false)}>
+        <FaSearch />
+      </Link>
+      <Link to={"/cart"} onClick={()=> setIsOpen(false)}>
+        <FaShoppingBag />
+      </Link>
+      {user?._id ? (
+        <>
+          <button onClick={()=> setIsOpen(prev => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
+            <div>
+              {user?.role === "admin" && (
+                <Link to={"/admin/dashboard"} onClick={()=> setIsOpen(false)}>Admin</Link>
+              )}
+
+              <Link to={"/orders"} onClick={()=> setIsOpen(false)}>Orders</Link>
+              <button onClick={logoutHandler}>
+                Sign Out <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -1,11 +1,31 @@
-
+import { Link } from "react-router-dom";
+import ProductCard from "../components/productCard";
 
 const Home = () => {
+  const addToCartHandler = () => {};
   return (
-    <div>
-      home
-    </div>
-  )
-}
+    <div className="home">
+      <section></section>
 
-export default Home
+      <h1>
+        Latest Products
+        <Link to={"/search"} className="findMore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        <ProductCard
+          photo="https://m.media-amazon.com/images/I/31ilq3hPhEL._SY445_SX342_QL70_FMwebp_.jpg"
+          productId="dfdasfdaf"
+          name="Mackbook"
+          price={4568}
+          stock={434}
+          handler={addToCartHandler}
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
