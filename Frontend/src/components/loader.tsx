13eeb72f9acd633a35b2ce726ab46399b981662b
JarmanjitSingh <jarmanjits176@gmail.com>
2024-03-07 22:49:21 +0530

    fetch all products and skeleton loader done

--- Frontend/src/components/loader.tsx
+++ Frontend/src/components/loader.tsx
@@ -1,11 +1,22 @@
-
-
 const Loader = () => {
-  return (
-    <div>
-      loading ...
-    </div>
-  )
+  return <div>loading ...</div>;
+};
+
+export default Loader;
+
+interface SkeletonProps {
+  width?: string;
+  length?: number;
 }
 
-export default Loader
+export const Skeleton = ({ width = "unset", length = 3 }: SkeletonProps) => {
+  const skeletons = Array.from({ length }, (_, index) => (
+    <div key={index} className="skeleton-shape"></div>
+  ));
+
+  return (
+    <div className="skeleton-loader" style={{ width }}>
+      {skeletons}
+    </div>
+  );
+};
--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -1,8 +1,8 @@
+import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { Skeleton } from "../components/loader";
 import ProductCard from "../components/productCard";
 import { useLatestProductsQuery } from "../redux/api/productApi";
-import toast from "react-hot-toast";
-import Loader from "../components/loader";
 
 const Home = () => {
   const { data, isLoading, isError } = useLatestProductsQuery("");
@@ -21,9 +21,10 @@ const Home = () => {
         </Link>
       </h1>
 
+      
       <main>
         {isLoading ? (
-          <Loader />
+          <Skeleton  width="80vw"/>
         ) : (
           data?.products.map((i) => (
             <ProductCard
